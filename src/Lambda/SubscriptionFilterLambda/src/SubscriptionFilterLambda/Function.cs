a0da22f4722ba783eb555992e3760c327c9789d4
tetsujin <tetsujin@tetsujin.dev>
2025-02-23 21:05:27 +0900

    SNS 用のメソッドを削除

--- src/Lambda/SubscriptionFilterLambda/src/SubscriptionFilterLambda/Function.cs
+++ src/Lambda/SubscriptionFilterLambda/src/SubscriptionFilterLambda/Function.cs
@@ -10,26 +10,10 @@ namespace SubscriptionFilterLambda;
 public class Function
 {
     private readonly TeamsWebhookService teamsWebhookService;
-    private readonly SimpleNotificationServiceService simpleNotificationServiceService;
-    private readonly string topicArn;
 
     public Function()
     {
-        // teamsWebhookService = new("https://prod-02.japaneast.logic.azure.com:443/workflows/a7052248f26f45b7b6e079ce7a32c352/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=EOuGFLDdCqgFcvEWocglARAbSac_34BWbLBjiJy0afA");
         teamsWebhookService = new TeamsWebhookService(Environment.GetEnvironmentVariable("teams_webhook_uri") ?? throw new ArgumentNullException("teams_webhook_uri"));
-        //simpleNotificationServiceService = new SimpleNotificationServiceService();
-        //topicArn = Environment.GetEnvironmentVariable("topic_arn") ?? throw new ArgumentNullException("topic_arn");
-    }
-
-    public string FunctionHandler(string input, ILambdaContext context)
-    {
-        return input.ToUpper();
-    }
-
-    public async Task<string> PublishFromSubscriptionFilterToChatbotAsync(SubsctiptionFilterInputModel input, ILambdaContext context)
-    {
-        context.Logger.LogLine(input.GetLogMessage());
-        return await simpleNotificationServiceService.PublishToTopicForChatbotAsync(topicArn, input.GetLogMessage());
     }
 
     public async Task PublishFromSubscriptionFilterToTeamsWebhookAsync(SubsctiptionFilterInputModel input, ILambdaContext context)
@@ -38,7 +22,7 @@ public class Function
         foreach (var logEvent in logEvents.EnumerateArray())
         {
             var message = logEvent.GetProperty("message").GetString();
-            
+
             if (message != null)
             {
                 await teamsWebhookService.SendMessageAsync(message);
